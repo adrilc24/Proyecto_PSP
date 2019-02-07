a123fc4bf394c8cf19a1f815844773b814d7f4ac
adrilc24 <adrilc24@gmail.com>
2019-02-07 09:26:07 +0100

    Actualizado ProyectoChat

--- ProyectoChat/src/serpis/psp/ClienteChat.java
+++ ProyectoChat/src/serpis/psp/ClienteChat.java
@@ -28,135 +28,140 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class ClienteChat extends JFrame implements ActionListener, Runnable{
+public class ClienteChat extends JFrame implements ActionListener, Runnable {
 
-private static final long serialVersionUID= 1L;
-Socket socket= null;
+	private static final long serialVersionUID = 1L;
+	Socket socket = null;
 
 //streams
-DataInputStream fentrada; //Para leer los mensajes
-DataOutputStream fsalida; //Para escribir los mensajes
+	DataInputStream fentrada; // Para leer los mensajes
+	DataOutputStream fsalida; // Para escribir los mensajes
 
-String nombre;
-static JTextField mensaje= new JTextField();
+	String nombre;
+	static JTextField mensaje = new JTextField();
 
-private JScrollPane scrollpane1;
-static JTextArea textarea1;
+	private JScrollPane scrollpane1;
+	static JTextArea textarea1;
 
-JButton botonEnviar= new JButton("Enviar");
-JButton botonSalir= new JButton("Salir");
-boolean repetir= true;
+	JButton botonEnviar = new JButton("Enviar");
+	JButton botonSalir = new JButton("Salir");
+	boolean repetir = true;
 
 //constructor
-public ClienteChat(Socket s,String nombre) {
-super("CONEXION DEL CLIENTE CHAT: "+nombre);
-setLayout(null);
+	public ClienteChat(Socket s, String nombre) {
+		super("CONEXION DEL CLIENTE CHAT: " + nombre);
+		setLayout(null);
 
-mensaje.setBounds(10,10,400,30);
-add(mensaje);
+		mensaje.setBounds(10, 10, 400, 30);
+		add(mensaje);
 
-textarea1= new JTextArea();
-scrollpane1= new JScrollPane(textarea1);
-scrollpane1.setBounds(10,50,400,300);
-add(scrollpane1);
+		textarea1 = new JTextArea();
+		scrollpane1 = new JScrollPane(textarea1);
+		scrollpane1.setBounds(10, 50, 400, 300);
+		add(scrollpane1);
 
-botonEnviar.setBounds(420,10,100,30);
-add(botonEnviar);
-botonSalir.setBounds(420,50,100,30);
-add(botonSalir);
+		botonEnviar.setBounds(420, 10, 100, 30);
+		add(botonEnviar);
+		botonSalir.setBounds(420, 50, 100, 30);
+		add(botonSalir);
 
-textarea1.setEditable(false);
-botonEnviar.addActionListener(this);
-botonSalir.addActionListener(this);
-setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		textarea1.setEditable(false);
+		botonEnviar.addActionListener(this);
+		botonSalir.addActionListener(this);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
-socket= s;
-this.nombre= nombre;
-try {
-fentrada= new DataInputStream(socket.getInputStream());
-fsalida= new DataOutputStream(socket.getOutputStream());
-String texto= ">Entra en el chat...."+nombre;
-fsalida.writeUTF(texto);
-}catch(IOException e) {
-System.out.println("Error de E/S");
-e.printStackTrace();
-System.exit(0);;
-}
+		socket = s;
+		this.nombre = nombre;
+		try {
+			fentrada = new DataInputStream(socket.getInputStream());
+			fsalida = new DataOutputStream(socket.getOutputStream());
+			String texto = ">Entra en el chat...." + nombre;
+			fsalida.writeUTF(texto);
+		} catch (IOException e) {
+			System.out.println("Error de E/S");
+			e.printStackTrace();
+			System.exit(0);
+			;
+		}
 
-}//Fin constructor
+	}// Fin constructor
 
 //accion cuando pulsamos botones
-public void actionPerformed(ActionEvent e) {
-if(e.getSource()== botonEnviar) { //Se pulsa ENVIAR
-if(mensaje.getText().trim().length()==0) {
-return;
-}
-String texto= nombre +">" + mensaje.getText();
-try {
-mensaje.setText("");
-fsalida.writeUTF(texto);
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == botonEnviar) { // Se pulsa ENVIAR
+			if (mensaje.getText().trim().length() == 0) {
+				return;
+			}
+			String texto = nombre + ">" + mensaje.getText();
+			try {
+				mensaje.setText("");
+				fsalida.writeUTF(texto);
 
-}catch(IOException e1) {
-	e1.printStackTrace();
-}
-}
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
 
-if (e.getSource() == botonSalir) {
-	String texto = " >Abandona el chat..." +nombre;
-}
-try {
-	fsalida.writeUTF(texto);
-	fsalida.writeUTF("*");
-	repetir = false;
-} catch (IOException e1) {
-	e1.printStackTrace();
-}
-}
-}
-public void run()String texto = "";
-while (repetir) {
-	try {
-		texto = fentrada.readUTF()
+		if (e.getSource() == botonSalir) {
+			String texto = " >Abandona el chat..." + nombre;
+
+			try {
+				fsalida.writeUTF(texto);
+				fsalida.writeUTF("*");
+				repetir = false;
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	} // FIN ACCIÓN BOTONES
+
+	public void run() {
+		String texto = "";
+
+		while (repetir) {
+			try {
+				texto = fentrada.readUTF();
 				textarea1.setText(texto);
-	} catch (IOException e) {
-		JOptionPane.showMessageDialog("IMPOSIBLE CONECTAR CON EL SERVIDOR\n"+ e.getMessage(), "<<MENSAJE DE ERROR:2>>", JOptionPane.ERROR_MESSAGE);
-		repetir = false;
-	}
-}
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, "IMPOSIBLE CONECTAR CON EL SERVIDOR\n" + e.getMessage(),
+						"<<MENSAJE DE ERROR:2>>", JOptionPane.ERROR_MESSAGE);
+				repetir = false;
+			}
+		}
 
-try {
-	socket.close();
-	System.exit(0);
-} catch (IOException e) {
-	e.printStackTrace();
-}
-}
-
-public static void main(String args[]) {
-	int puerto = 44444;
-	Socket s = null;
-	
-	String nombre = JOptionPane.showInputDialog("Introduce tu nombre aquí: ");
-	
-	if (nombre.trim().length() == 0) {
-		System.out.println("El nombre está vacío....");
-		return;
+		try {
+			socket.close(); // cerrar socket
+			System.exit(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	try {
-		s = new Socket("localhost", puerto);
-	
+
+	public static void main(String args[]) {
+		int puerto = 44444;
+		Socket s = null;
+
+		String nombre = JOptionPane.showInputDialog("Introduce tu nombre aquí: ");
+
+		if (nombre.trim().length() == 0) {
+			System.out.println("El nombre está vacío....");
+			return;
+		}
+
+		try {
+			s = new Socket("localhost", puerto);
 
 ///////
 
 //167
-ClienteChat cliente= new ClienteChat(s,nombre);
-cliente.setBounds(0,0,540,400);
-cliente.setVisible(true);
-new Thread(cliente).start; //Lanzar hilo cliente
+			ClienteChat cliente = new ClienteChat(s, nombre);
+			cliente.setBounds(0, 0, 540, 400);
+			cliente.setVisible(true);
+			new Thread(cliente).start(); // Lanzar hilo cliente
 
-}catch(IOException e) {
-JOptionPane.showMessageDialog(null, "IMPOSIBLE CONECTAR CON EL SERVIDOR\n" + e.getMessage(), "<<MENSAJE DE ERROR:1>>", JOptionPane.ERROR_MESSAGE);
-
-}//Cliente Chat
-
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "IMPOSIBLE CONECTAR CON EL SERVIDOR\n" + e.getMessage(),
+					"<<MENSAJE DE ERROR:1>>", JOptionPane.ERROR_MESSAGE);
+		}
+	}// main
+}// Cliente Chat
